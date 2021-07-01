d358fd38928fcf2a43ae8c722d15f69ac4915bf9
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-06-30 20:00:59 -0400

    added psuedo code to js file

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// question variable with quetions, choices and answer
+var problem= ['one', 'two', 'three']
+
+for (let i = 0; i < problem.length; i++) {
+    ;
+  }
+  console.log(problem)
+
+
+// start button 
+// eventlister on click to display instructions for the quiz
+// begin quiz event listener
+// loop through question array
+// Save high score to local storage event listener
+// Timer that starts on question display and resets on the start of the next question
