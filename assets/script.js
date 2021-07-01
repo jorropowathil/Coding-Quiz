d0b66f4ce1d8f576628a49f9865fb2823b2365d4
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-06-30 21:46:36 -0400

    added start button dissapearing function on click

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,8 @@
 // question variable with quetions, choices and answer
-var problem= ['one', 'two', 'three']
+var problem = ['one', 'two', 'three'];
+var startButtonEl = document.getElementById("start");
 
-for (let i = 0; i < problem.length; i++) {
-    ;
-  }
-  console.log(problem)
-
-
-// start button 
-// eventlister on click to display instructions for the quiz
-// begin quiz event listener
-// loop through question array
-// Save high score to local storage event listener
-// Timer that starts on question display and resets on the start of the next question
+startButtonEl.addEventListener("click", function() { 
+  console.log("Hello!");
+  startButtonEl.style.display = "none";
+})
