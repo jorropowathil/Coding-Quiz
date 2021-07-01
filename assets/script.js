b523d0f4befcc4e6b79cd9b18a55650082bbcc99
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-06-30 22:06:46 -0400

    added more questions and commented out code

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,42 @@
 // question variable with quetions, choices and answer
-var problem = ['one', 'two', 'three'];
+var problem = [
+  {
+  question: "What does CSS stand for?",
+  options: ["Colorful style sheet", "Cool style sheet", "Cascading style sheet", "Comical styling slides"],
+  answer: 2
+}, 
+{
+  question: "What does HTML stand for?",
+  options: ["hypertext markdown language","answer b","answer c", "answer d"],
+  answer: 1
+},
+{
+  question: "Question 3",
+  options: ["answer a","answer b","answer c", "answer d"],
+  answer: 1
+},
+{
+  question: "Question 4",
+  options: ["answer a","answer b","answer c", "answer d"],
+  answer: 1
+},
+{
+  question: "Question 5",
+  options: ["answer a","answer b","answer c", "answer d"],
+  answer: 1
+},
+];
+
+
 var startButtonEl = document.getElementById("start");
+// var answerChoices= document.getElementsByClassNames("choice");
 
 startButtonEl.addEventListener("click", function() { 
   console.log("Hello!");
   startButtonEl.style.display = "none";
 })
+
+// function nextQuestion(){
+
+// }
+// Next 
